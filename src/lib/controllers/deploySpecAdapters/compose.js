7af92675596da73d279ea54968f6643732661d7e
Michael Angelos Simos <michalis@resin.io>
2018-08-16 16:10:06 +0300

    deploySpec: Add config validation for kubernetes adapter
    Change-type: minor
    Signed-off-by: Michael Angelos-Simos <michalis@resin.io>

--- src/lib/controllers/deploySpecAdapters/compose.js
+++ src/lib/controllers/deploySpecAdapters/compose.js
@@ -3,30 +3,42 @@ const Promise = require('bluebird')
 const { readFileAsync, writeFileAsync, ensureDirAsync } = Promise.promisifyAll(require('fs-extra'))
 const mustache = require('mustache')
 const path = require('path')
+const _ = require('lodash')
 const getConfig = require('./getConfigCompose')
+const configValidator = require('../configValidator/configValidator')
 
-const generateDeploySpecFile = (templatePath, configPath, version, archiveStore) => {
+const generateDeploySpecFile = (templatePath, configPath, configManifestPath, version, archiveStore) => {
 	return getConfig(configPath)
 		.then(config => {
-			return readFileAsync(templatePath, 'utf8')
-				.then(template => {
-					let output = mustache.render(template, config)
-					let outputPath = path.join(
-						archiveStore,
-						version,
-						'docker-compose',
-						path.basename(templatePath).replace('.tpl.','.')
-					)
-					return ensureDirAsync(path.dirname(outputPath))
-						.then(() => {
-							return writeFileAsync(outputPath, output)
+			return new configValidator(config, configManifestPath).validate().then((errors) => {
+				if (errors){
+					let errorList = []
+					_.forEach(errors, err => {
+						errorList.push(err.stack)
+					})
+					return errorList
+				}
+				else {
+					return readFileAsync(templatePath, 'utf8')
+						.then(template => {
+							let output = mustache.render(template, config)
+							let outputPath = path.join(
+								archiveStore,
+								version,
+								'docker-compose',
+								path.basename(templatePath).replace('.tpl.', '.')
+							)
+							return ensureDirAsync(path.dirname(outputPath))
+								.then(() => {
+									return writeFileAsync(outputPath, output)
+								})
 						})
-				})
-
+				}
 		})
 		.catch(err => {
 			return err.message
 		})
+	})
 }
 
 module.exports = generateDeploySpecFile
