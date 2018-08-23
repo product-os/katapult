9f56c6227f7d8d30d35b322e27d21120cba278f6
Michael Angelos Simos <michalis@resin.io>
2018-08-23 12:56:19 +0300

    autoGenerator: Add GENERATE_CERT
    Change-type: minor
    Signed-off-by: Michael Angelos Simos <michalis@resin.io>

--- src/lib/controllers/configAutoGenerator/autoGeneratorPlugins/all.js
+++ src/lib/controllers/configAutoGenerator/autoGeneratorPlugins/all.js
@@ -3,3 +3,5 @@
 module.exports['GENERATE_API_KEY'] = require('./generateApiKey')
 module.exports['GENERATE_API_KEY_16'] = require('./generateApiKey16')
 module.exports['GENERATE_CA'] = require('./generateCA')
+module.exports['GENERATE_CERT'] = require('./generateCert')
+
