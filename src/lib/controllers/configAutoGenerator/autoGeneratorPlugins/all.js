c99f2512aafd528a71e9981ddda470eb7d89efea
Michael Angelos Simos <michalis@resin.io>
2018-08-23 10:25:54 +0300

    autoGenerator: Add GENERATE_CA
    Change-type: minor
    Signed-off-by: Michael Angelos Simos <michalis@resin.io>

--- src/lib/controllers/configAutoGenerator/autoGeneratorPlugins/all.js
+++ src/lib/controllers/configAutoGenerator/autoGeneratorPlugins/all.js
@@ -2,3 +2,4 @@
 
 module.exports['GENERATE_API_KEY'] = require('./generateApiKey')
 module.exports['GENERATE_API_KEY_16'] = require('./generateApiKey16')
+module.exports['GENERATE_CA'] = require('./generateCA')
