59242f2e8c29c3e0e76ea3f80f5de310dd487753
Michael Angelos Simos <michalis@resin.io>
2018-10-03 20:45:27 +0300

    AutoGenerators: Add generatePublicKey (from private)
    Change-type: patch
    Signed-off-by: Michael Angelos Simos <michalis@resin.io>

---
+++ src/lib/controllers/configAutoGenerator/autoGeneratorPlugins/generatePublicKey.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const forge = require('node-forge')
+
+
+/**
+ *
+ * @param privateKeyPEM: PEM format private key string.
+ * @returns PublicKeyPEM.
+ */
+
+let generatePublicKey = (privateKeyPEM) => {
+	let privateKey = forge.pki.privateKeyFromPem(privateKeyPEM)
+	let pubKey = forge.pki.rsa.setPublicKey(privateKey.n, privateKey.e)
+	return Buffer.from(forge.pki.publicKeyToPem(pubKey)).toString()
+}
+
+module.exports = generatePublicKey
