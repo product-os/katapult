df798ce5354234e8033584894fb7c1a19c111463
Michael Angelos Simos <michalis@resin.io>
2018-10-04 09:29:21 +0300

    Update autoGeneratorPlugins list
    Change-type: patch
    Signed-off-by: Michael Angelos Simos <michalis@resin.io>

--- src/lib/controllers/configAutoGenerator/autoGeneratorPlugins/all.js
+++ src/lib/controllers/configAutoGenerator/autoGeneratorPlugins/all.js
@@ -2,7 +2,9 @@
 
 module.exports['GENERATE_API_KEY'] = require('./generateApiKey')
 module.exports['GENERATE_API_KEY_16'] = require('./generateApiKey16')
-module.exports['GENERATE_CA'] = require('./generateCA')
+module.exports['GENERATE_CA_CERT'] = require('./generateCACert')
 module.exports['GENERATE_CERT'] = require('./generateCert')
 module.exports['GENERATE_CERT_CHAIN'] = require('./generateCertChain')
 module.exports['GENERATE_CHAIN'] = require('./generateChain')
+module.exports['GENERATE_PRIVATE_KEY'] = require('./generatePrivateKey')
+module.exports['GENERATE_PUBLIC_KEY'] = require('./generatePublicKey')
