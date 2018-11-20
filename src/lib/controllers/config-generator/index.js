129a4c4676367e2ff469d254c73355f1729859d2
Michael Angelos Simos <michalis@resin.io>
2018-11-20 10:25:38 +0200

    config-generator: Refactor and add interactive mode
    Change-type: minor
    Signed-off-by: Michael Angelos Simos <michalis@balena.io>

--- src/lib/controllers/config-generator/index.js
+++ src/lib/controllers/config-generator/index.js
@@ -2,6 +2,9 @@
 const Promise = require('bluebird')
 const _ = require('lodash')
 const Validator = require('jsonschema').Validator
+const inquirer = require('inquirer')
+const ConfigValidator = require('../config-validator')
+
 const {
 	GENERATE_API_KEY,
 	GENERATE_API_KEY_16,
@@ -15,45 +18,105 @@ const {
 } = require('./plugins/all')
 const { escape, base64decode, base64 } = require('./filter-functions')
 
-module.exports = class ConfigAutoGenerator {
-	constructor(config, configManifest) {
+module.exports = class ConfigGenerator {
+	constructor({ config, configManifest, mode }) {
 		this.config = config
 		this.configManifest = configManifest
+		this.mode = mode
+	}
+
+	/**
+	 * Validates property against this.configManifest
+	 * @param name: Property name
+	 * @param value: Property value
+	 * @returns: {boolean true|error string}
+	 */
+	validateProperty(name, value) {
+		let validator = new Validator()
+		let schemaErrors = validator.validate(
+			{
+				[name]: value
+			},
+			{
+				type: 'object',
+				properties: {
+					[name]: _.get(this.configManifest.properties, name)
+				},
+				required: [name]
+			}
+		)
+		if (schemaErrors.valid) return true
+		else return schemaErrors.errors[0].stack
 	}
 
 	generate() {
 		Object.assign(global, this.config)
-		let validator = new Validator()
-		let promiseChain = Promise.resolve()
-		_.forEach(this.configManifest.properties, (value, name) => {
-			let formula = _.get(value, ['default', 'eval'])
-			if (formula) {
-				let invalid =
-					(_.has(this.config, name) || _.get(value, 'required')) &&
-					validator.validate(
-						{
-							[name]: this.config[name]
-						},
-						{
-							type: 'object',
-							properties: { [name]: value },
-							required: [name]
-						}
-					).errors.length
 
-				if (invalid) {
-					promiseChain = promiseChain
-						.then(() => {
-							console.log('Generating', name)
-							return eval(formula.split('\n').join(''))
-						})
-						.then(result => {
-							global[name] = result
-							this.config[name] = result
-						})
+		return new ConfigValidator(this.config, this.configManifest)
+			.validate(false)
+			.then(errors => {
+				let invalidProperties = _.reduce(
+					errors,
+					(result, o) => {
+						result[_.replace(o.property, /^instance\./g, '')] = o
+						return result
+					},
+					{}
+				)
+				if (this.mode === 'aggressive') {
+					return this.generateAgressively(invalidProperties)
+				} else if (this.mode === 'interactive') {
+					return this.generateInteractively(invalidProperties)
 				}
-			}
+			})
+	}
+
+	generateAgressively(invalidProperties) {
+		let promiseChain = Promise.resolve()
+		_.forEach(invalidProperties, (error, propertyName) => {
+			promiseChain = promiseChain
+				.then(() => {
+					let formula = _.get(this.configManifest, [
+						'properties',
+						propertyName,
+						'default',
+						'eval'
+					])
+					if (formula) {
+						console.log('Generating', propertyName)
+						return eval(formula.split('\n').join(''))
+					} else {
+						throw new Error(error.stack)
+					}
+				})
+				.then(result => {
+					global[propertyName] = result
+					this.config[propertyName] = result
+				})
 		})
-		return promiseChain.return(this.config)
+		return promiseChain.then(() => {
+			return this.config
+		})
+	}
+
+	generateInteractively(invalidProperties) {
+		let questions = []
+		_.forEach(invalidProperties, (error, propertyName) => {
+			questions.push({
+				message: error.stack,
+				type: 'input',
+				name: propertyName,
+				validate: value => {
+					return this.validateProperty(propertyName, value)
+				}
+			})
+		})
+		return inquirer.prompt(questions).then(values => {
+			_.forEach(values, (value, name) => {
+				global[name] = value
+				this.config[name] = value
+			})
+			return this.config
+		})
 	}
 }
